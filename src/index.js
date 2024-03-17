b3a482eb3a3466f96acfe9dae89594a305ac003a
Manh8008 <125587030+Manh8008@users.noreply.github.com>
2024-03-17 20:40:06 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -2,27 +2,29 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 // const handlebars = require('express-handlebars');
-const {engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+const route = require('./router');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Cái trung gian để xử lí dữ liệu từ form submit lên
+// Dưới dạng form gửi lên thì có cái  app.use(express.urlencoded()) xử lí
+app.use(express.urlencoded({ extended: true }));
+// Gửi từ code js lên thì có app.use(express.json()) xử lí
+app.use(express.json());
 
 // Sử dụng HTTP logger
 app.use(morgan('combined'));
 
-app.engine('hbs', engine({extname: '.hbs'}));
+app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-// Định nghĩa route cho trang chủ
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+//Routes init
+route(app);
 
 // Lắng nghe các kết nối đến cổng port
 app.listen(port, () => {
